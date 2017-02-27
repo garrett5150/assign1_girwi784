348d9e3960521c24f2c7f9a61c5ded4d1490f98b
Garrett Irwin <garrettirwin@hotmail.com>
2017-02-26 17:10:21 -0700

    can send input from html page to app.js and then pass it to auth.js

--- application/app.js
+++ application/app.js
@@ -6,17 +6,61 @@ var app = express();
 var fs = require('fs');
 var bodyParse = require('body-parser');
 app.use(express.static("public"));
-app.use(bodyParse.urlencoded({extended:false}));
+app.use(bodyParse.urlencoded({extended: false}));
+var http = require('http');
+var querystring = require('querystring');
 
-app.post('/login', function(req, res)
-{
-   var username = req.body.un;
-   var pw = req.body.pw;
-   console.log("username: " + username + ", pw: " + pw);
+
+app.post('/login', function (req, res) {
+    var post_data = querystring.stringify({
+        'login': req.body.un,
+        'pw': req.body.pw
+    });
+
+    PostCode(post_data);
+
 });
 
 
-var server = app.listen(3001,function(){
+function PostCode(post_data) {
+
+
+    var post_options = {
+        host: 'localhost',
+        port: '3002',
+        path: '/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(post_data)
+        }
+    };
+
+    var post_req = http.request(post_options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+
+    post_req.write(post_data);
+    post_req.end();
+}
+
+
+
+/*app.post('/login', function(req, res)
+ {
+ var response = {
+ login:req.body.un,
+ pw:req.body.pw
+ };
+ console.log(response);
+ res.end(JSON.stringify(response));
+ });*/
+
+
+var server = app.listen(3001, function () {
     var port = server.address().port;
     console.log("Server Started at http://localhost:%s", port);
 });
--- authentication/auth.js
+++ authentication/auth.js
@@ -7,13 +7,22 @@ var fs = require('fs');
 var bodyParse = require('body-parser');
 app.use(bodyParse.urlencoded({extended:false}));
 
+/*app.use(function(req, res)
+{
+   res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+   //res.set('Access-Control-Allow-Origin', 'http://localhost:3001');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    /!*app.post('/login', function(req, res)
+    {
+        var username = req.body.un;
+        var pw = req.body.pw;
+        console.log("username: " + username + ", pw: " + pw);
+    });*!/
+});*/
+
 app.post('/login', function(req, res)
 {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3001');
-
-    var username = req.body.un;
-    var pw = req.body.pw;
-    console.log("username: " + username + ", pw: " + pw);
+    console.log('Login: ' + req.body.login + ' Password: ' + req.body.pw);
 });
 
 
